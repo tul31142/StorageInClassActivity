4c4f6d53b2e23aa34ec0e2a18212e1e836c1e9a1
Alexis Graham <tul31142@temple.edu>
2026-04-07 12:17:54 -0400

    Finished in class activity!

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -14,6 +14,11 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileOutputStream
+import java.io.FileReader
+import java.io.IOException
 
 // TODO (1: Fix any bugs)
 // TODO (2: Add function saveComic(...) to save comic info when downloaded
@@ -28,6 +33,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var showButton: Button
     lateinit var comicImageView: ImageView
 
+    lateinit var myFile: File
+    private val comicFile :String  = "my_comic"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -40,8 +48,32 @@ class MainActivity : AppCompatActivity() {
         showButton = findViewById<Button>(R.id.showComicButton)
         comicImageView = findViewById<ImageView>(R.id.comicImageView)
 
+        myFile = File(filesDir, comicFile)
+
+        if(myFile.exists())
+        {
+            try{
+                val br = BufferedReader(FileReader(comicFile))
+                val text = StringBuilder()
+                var line: String?
+                while(br.readLine().also{
+                        line = it
+                    } != null){
+                    text.append(line)
+                }
+
+                br.close()
+                showComic(JSONObject(text.toString()))
+            }
+            catch(e: IOException){
+                e.printStackTrace()
+            }
+        }
+
         showButton.setOnClickListener {
             downloadComic(numberEditText.text.toString())
+
+
         }
 
     }
@@ -51,7 +83,8 @@ class MainActivity : AppCompatActivity() {
         val url = "https://xkcd.com/$comicId/info.0.json"
         requestQueue.add (
             JsonObjectRequest(url
-                , {showComic(it)}
+                , {saveComic(it)
+                    showComic(it)}
                 , {}
             )
         )
@@ -66,8 +99,15 @@ class MainActivity : AppCompatActivity() {
 
     // Implement this function
     private fun saveComic(comicObject: JSONObject) {
+        try {
+            val outputStream = FileOutputStream(myFile)
+            outputStream.write(comicObject.toString().toByteArray())
+            outputStream.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 
     }
 
 
-}
